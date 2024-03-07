56ff82a21d29ebb5765d748889bcdd3ce489692a
gabhiaf98 <gabhiaf98@gmail.com>
2024-03-07 06:03:30 +0000

    Charts implemented

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,1 @@
-const sidebarButton = document.querySelector(".sidebar-button");
-const sidebar = document.querySelector("#logo-sidebar");
 
-sidebarButton.addEventListener("click", () => {
-  sidebar.classList.toggle("translate-x-full");
-});
